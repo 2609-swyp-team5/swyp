8994df307b86acdcdcd491f943e5d1e3a58fcaa3
codes-gy <podojjang2301@koreacu.ac.kr>
2026-09-13 15:05:51 +0900

    refactor: 불필요한 설명 주석 제거

--- backend/src/main/java/com/swyp/team5/BackendApplication.java
+++ backend/src/main/java/com/swyp/team5/BackendApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
 
-/** JWT 기반 자체 인증만 사용하므로 Spring Security의 기본 폼로그인용 자동 계정 생성은 비활성화한다. */
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class BackendApplication {
 
